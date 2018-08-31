76caaa1fcb20b333c410b5f12a87f4be8b0e70eb
sorrycc <sorrycc@gmail.com>
2018-08-31 17:59:29 +0800

    Update

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
 
+const { existsSync } = require('fs');
+const { join } = require('path');
 const semver = require('semver');
 const chalk = require('chalk');
+const mkdirp = require('mkdirp');
+const clipboardy = require('clipboardy');
 
 if (!semver.satisfies(process.version, '>= 8.0.0')) {
-  console.error(chalk.red('The generator will only work with Node v8.0.0 and up!'));
+  console.error(chalk.red('✘ The generator will only work with Node v8.0.0 and up!'));
   process.exit(1);
 }
+
+const script = process.argv[2];
+if (script === '-v' || script === '--version') {
+  console.log(require('../package').version);
+  if (existsSync(join(__dirname, '../.local'))) {
+    console.log(chalk.cyan('@local'));
+  }
+  process.exit(0);
+}
+
+if (!script) {
+  console.error(chalk.red('✘ App name should be specified'));
+  process.exit(1);
+}
+
+mkdirp.sync(script);
+process.chdir(script);
 
 const BasicGenerator = require('../lib/BasicGenerator');
 const generator = new BasicGenerator(process.argv.slice(2), {
@@ -17,5 +38,7 @@ const generator = new BasicGenerator(process.argv.slice(2), {
   resolved: __dirname,
 });
 generator.run(() => {
-  console.log('✨ Done');
+  clipboardy.writeSync(`cd ${script}`);
+  console.log('📋  Copied to clipboard, just use Ctrl+V');
+  console.log('✨  File Generate Done');
 });

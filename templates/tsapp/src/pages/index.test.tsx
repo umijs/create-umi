9a6390f35dbb9f451a7157b710e4d16a3fb9203a
xiaohuoni <448627663@qq.com>
2018-11-21 15:36:58 +0800

    :sparkles: add jest case

--- templates/tsapp/src/pages/index.test.tsx
+++ templates/tsapp/src/pages/index.test.tsx
@@ -1,4 +1,29 @@
-//后续增加
-it('test', () => {
-  expect(1).toBe(1)
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import App from "./index";
+import { any } from "prop-types";
+
+const mockStore = configureMockStore();
+const store = mockStore({
+  global: {
+    message: "umi"
+  }
+});
+const props = {
+  dispatch: any,
+  location: any,
+  message: any
+};
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App {...props} />
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });

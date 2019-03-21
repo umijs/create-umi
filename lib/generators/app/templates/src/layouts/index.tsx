34e1cd5ac7121deda7d77ef05ebd6f468a9168b5
imhele <work@imhele.com>
2019-03-21 11:57:38 +0800

    chore: remove BasicLayoutProps in template

--- lib/generators/app/templates/src/layouts/index.tsx
+++ lib/generators/app/templates/src/layouts/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import styles from './index.css';
 
-interface BasicLayoutProps {
-  history?: History;
-  location?: Location;
-}
-
-const BasicLayout: React.FC<BasicLayoutProps> = props => {
+const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
       <h1 className={styles.title}>Yay! Welcome to umi!</h1>

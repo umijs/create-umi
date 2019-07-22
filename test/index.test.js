6d1695bab6652a6537af784347a7dc76fbcb807c
DiamondYuan <admin@diamondyuan.com>
2019-07-22 16:42:36 +0800

    test: update test script

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,29 @@
 const coffee = require('coffee');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-describe('cli', () => {
-  it('should fork node cli', () => {
-    return coffee
-      .fork('./cli.js')
+describe('test umi-create', () => {
+  it('test generate antd pro project', () => {
+    const fixtures = fs.mkdtempSync(path.join(os.tmpdir(), `umi-create`));
+    const Coffee = coffee.Coffee;
+
+    return new Coffee({
+      method: 'fork',
+      cmd: path.join(__dirname, '../cli.js'),
+      opt: { cwd: fixtures },
+    })
       .debug()
+      .expect('stdout', /Select the boilerplate type ant-design-pro/)
+      .waitForPrompt()
       .write('\n')
-      .expect('stdout', new RegExp('Select the boilerplate type ant-design-pro'))
+      .waitForPrompt()
+      .write('\n')
       .expect('code', 0)
+      .expect(
+        'stdout',
+        new RegExp('git clone https://github.com.cnpmjs.org/ant-design/ant-design-pro --depth=1'),
+      )
       .end();
   });
 });

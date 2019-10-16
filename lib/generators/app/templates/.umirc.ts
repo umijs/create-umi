9df976ff766e9ba35ea041f55b604e2c161c948d
信鑫-King <45808948@qq.com>
2019-10-16 12:08:50 +0800

    feat: app template routes use configuration (#87)

--- lib/generators/app/templates/.umirc.ts
+++ lib/generators/app/templates/.umirc.ts
@@ -3,6 +3,15 @@ import { IConfig } from 'umi-types';
 // ref: https://umijs.org/config/
 const config: IConfig =  {
   treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {

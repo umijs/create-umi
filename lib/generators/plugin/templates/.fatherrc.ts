1a7a3e282d079d5057b250f42328f91c02f04ded
xiaohuoni <448627663@qq.com>
2020-04-29 15:42:25 +0800

    feat: Upgrade to Umi 3

--- lib/generators/plugin/templates/.fatherrc.ts
+++ lib/generators/plugin/templates/.fatherrc.ts
@@ -1,18 +1,31 @@
-
-export default [
-  {
-    cjs: 'babel',
-  },
+export default [{
+  target: 'node',
+  cjs: { type: 'babel', lazy: true },
+  disableTypeCheck: true,
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      { libraryName: 'antd', libraryDirectory: 'es', style: true },
+      'antd',
+    ],
+  ],
 <% if (withUmiUI) { -%>
-  {
-    entry: 'ui/index.tsx',
-    typescriptOpts: {
-      check: false,
-    },
-    umd: {
-      name: '<%= name %>',
-      minFile: false,
+},
+{
+  entry: 'ui/index.tsx',
+  umd: {
+    name: 'tasks',
+    minFile: isProd,
+    sourcemap: !isProd,
+  },
+  extraExternals: ['antd', 'react', 'react-dom', 'xterm'],
+  typescriptOpts: {
+    check: false,
+    globals: {
+      antd: 'window.antd',
+      react: 'window.React',
+      'react-dom': 'window.ReactDOM',
     },
   },
 <% } -%>
-];
+}];
--- lib/generators/plugin/templates/example/pages/index.tsx
+++ lib/generators/plugin/templates/example/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import styles from './index.css';
 
-export default function() {
+export default () => {
+
   return (
     <div className={styles.normal}>
-      <h1>Page index</h1>
+      Hello Umi!
     </div>
   );
-}
+};
